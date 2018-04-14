56fe69ec0d0c3554d5dc85181403245538b784cf
amoyag00 <amoyag00@estudiantes.unileon.es>
2018-04-14 19:33:12 +0200

    modified concessionaire Home and stylesheet.

--- src/concessionaireHome.php
+++ src/concessionaireHome.php
@@ -17,8 +17,27 @@
             ?>
         </header>
         <div id="search-cart">
-            <button id="cart" src="img/cart.png"><span>2</span></button>
+            <button id="cart" src="img/cart.png"><span>0</span></button>
             <input type="text" id="search" placeholder="buscar" />
+            <div id="dropdown">
+                <table id="dropdown-table">
+                    <tr id="header-row">
+                        <th>Nombre</th>
+                        <th>Precio</th>
+                        <th>Cantidad</th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <td>Total: </td>
+                        <td id="total-price"></td>
+                        
+                    </tr>
+                       
+                </table>
+                
+                
+                <button id="pedir">Realizar pedido</button>
+            </div>
         </div>
        
         <nav id="conc-nav">
@@ -27,10 +46,10 @@
         </nav>
          <div id="productos-pedidos">
             <div class="producto" id="1">
-                <span>nombre producto</span>
-                Cantidad<input type="number"/>
-                <span>precio €</span>
-                <button type="button" class="pedido-button"> Añadir al carro</button>
+                <span class="product-name">nombre producto</span>
+                Cantidad<input type="number" value="0" class="product-quantity"/>
+                <span class="product-price">precio €</span>
+                <button type="button" class="add-cart"> Añadir al carro</button>
             </div>
         </div>
     </body>
--- src/script.js
+++ src/script.js
@@ -1,8 +1,86 @@
 $(document).ready(function(){
      $('#realizar-pedido').on("click",pedidoHandler);
+     $('#cart').on("click",showDropdown);
+     $('.add-cart').on("click",addToCart);
+     $('#dropdown-table').on("click",".delete-button",deleteFromCart);
     
 });
+
+var cartItems=new Array();
+var searchedItems=new Array();
+searchedItems.push(new item(1,"g-34 motor","very good",5,"tesla"));
+var currentPrice=0.0;
 
 function pedidoHandler(){
    $('#cart').css('visibility','visible');
 }
+
+function purchasedItem(id, quantity){
+    this.id=id;
+    this.quantity=quantity;
+}
+
+function item(id, name, features, price, provider){
+    this.id=id;
+    this.name=name;
+    this.features=features;
+    this.price=price;
+    this.provider=provider;
+}
+
+function showDropdown(){
+    if($('#dropdown').css("display")==="block"){
+        $('#dropdown').css("display","none");
+    }else{
+        $('#dropdown').css("display","block");
+    }
+
+}
+
+function addToCart(){
+   
+    var id=$(this).parent().attr('id');
+    var item=getItemOfId(id, searchedItems);
+    var name=item.name;
+    var quantity=$(this).prevAll().eq(1).val();
+    var price=item.price;
+    
+    $('#dropdown-table #header-row').after(
+            ' <tr>'+
+                    '<td>'+name+'</td>'+
+                    '<td>'+price+'€</td>'+
+                    '<td>'+quantity+'</td>'+
+                    '<td><button id='+id+' class="delete-button">X</button></td>'+
+                    '</tr>');
+     currentPrice+=price*quantity;
+    $('#dropdown-table #total-price').html(currentPrice+" €");
+    cartItems.push(new purchasedItem(id, quantity));
+    $('#cart span').text(cartItems.length);
+    
+}
+
+function deleteFromCart(){
+    var id=$(this).attr("id");
+    var purchasedItem=getItemOfId(id,cartItems);
+    var quantity=purchasedItem.quantity;
+    var price=getItemOfId(id,searchedItems).price;
+   $(this).parent().parent().remove();
+    for (var i=0;i<cartItems.length;i++){
+        if(cartItems[i].id===id){
+            cartItems.splice(i,1);
+        }
+    }
+    currentPrice-=price*quantity;
+    $('#dropdown-table #total-price').html(currentPrice + "€");
+     $('#cart span').text(cartItems.length);
+}
+
+function getItemOfId(id, array){
+    for (var i=0;i<array.length;i++){
+        if(array[i].id==id){/* dont add another = */
+            
+            return array[i];
+        }
+    }
+}
+
