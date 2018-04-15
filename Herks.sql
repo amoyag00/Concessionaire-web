16f006c1fc763baf0fce13657fe0764f2dc981d3
amoyag00 <amoyag00@estudiantes.unileon.es>
2018-04-15 23:36:55 +0200

    Add files via upload
    Added inserts and type attr in Usuario

--- Herks.sql
+++ Herks.sql
@@ -2,6 +2,7 @@
 CREATE TABLE Usuario(
 nombre VARCHAR(15),
 contrasena VARCHAR(15) NOT NULL,
+tipo VARCHAR(15) NOT NULL,
 PRIMARY KEY(nombre)
 );
 
@@ -55,6 +56,42 @@ FOREIGN KEY(producto_id) REFERENCES Producto(producto_id),
 FOREIGN KEY(pedido_id) REFERENCES Pedido(pedido_id)
 );
 
+INSERT INTO Usuario VALUES('prov','prov','provider');
+INSERT INTO Proveedor VALUES('prov');
+
+INSERT INTO Usuario VALUES('prov2','prov2','provider');
+INSERT INTO Proveedor VALUES('prov2');
+
+INSERT INTO Usuario VALUES('seat','seat','concessionaire');
+INSERT INTO Concesionario VALUES('seat');
+
+INSERT INTO Usuario VALUES('audi','audi','concessionaire');
+INSERT INTO Concesionario VALUES('audi');
+
+INSERT INTO Usuario VALUES('admin','admin','administrator');
+INSERT INTO Administrador VALUES('admin');
+
+INSERT INTO Producto(nombrePro,nombre,caracteristicas,precio,disponible) VALUES('prov','seat panda','gasolina',8000,1);
+set @idSeatPanda=LAST_INSERT_ID();
+INSERT INTO Producto(nombrePro,nombre,caracteristicas,precio,disponible) VALUES('prov','seat cebra','diesel',9000,1);
+set @idSeatCebra=LAST_INSERT_ID();
+INSERT INTO Producto(nombrePro,nombre,caracteristicas,precio,disponible) VALUES('prov','seat leon','gasolina',7000,1);
+
+INSERT INTO Producto(nombrePro,nombre,caracteristicas,precio,disponible) VALUES('prov2','audi panda','gasolina',17000,1);
+set @idAudiPanda=LAST_INSERT_ID();
+INSERT INTO Producto(nombrePro,nombre,caracteristicas,precio,disponible) VALUES('prov2','audi cebra','diesel',19000,1);
+INSERT INTO Producto(nombrePro,nombre,caracteristicas,precio,disponible) VALUES('prov2','audi leon','gasolina',18000,1);
+set @idAudiLeon=LAST_INSERT_ID();
+
+INSERT INTO Pedido(nombreCon,fecha,estado) VALUES('seat','2018-04-15',1);
+set @idPedidoSeat=LAST_INSERT_ID();
+INSERT INTO ListaProductos  VALUES(@idSeatCebra,@idPedidoSeat,3);
+INSERT INTO ListaProductos  VALUES(@idSeatPanda,@idPedidoSeat,10);
+
+INSERT INTO Pedido(nombreCon,fecha,estado) VALUES('audi','2018-04-16',1);
+set @idPedidoAudi=LAST_INSERT_ID();
+INSERT INTO ListaProductos  VALUES(@idAudiPanda,@idPedidoAudi,5);
+INSERT INTO ListaProductos  VALUES(@idAudiLeon,@idPedidoAudi,4);
 
 
 
