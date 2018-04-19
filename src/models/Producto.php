a59f12a8d74f90039e417a773c7e8ebfe7b4254e
amoyag00 <amoyag00@estudiantes.unileon.es>
2018-04-20 01:34:37 +0200

    moved Producto.php to models folder

--- src/models/Producto.php
+++ src/models/Producto.php
@@ -1,33 +1,46 @@
 <?php
-include 'DBConnection.php';
-$p=new Producto();
- $p->filter("motor");
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+require_once 'DBConnection.php';
 class Producto{
-    private $conn;
+    private $connection;
+    private $producto_id;
+    private $proveedor;
+    private $nombre;
+    private $caracteristicas;
+    private $precio;
+    private $disponible;
     
-    function __construct(){
-        $this->conn=DBConnection::getConnection();
+    function __construct() {
+        $this->connection = DBConnection::getConnection();
     }
     
-    function filter($pattern){
-        $statement=$this->conn->prepare("SELECT * FROM Producto WHERE nombre LIKE ?");
-        $regexp="%".$pattern."%";
-        $statement->bind_param("s",$regexp);
+    function setAttributes($data){
+        $this->proveedor = $data["proveedor"];
+        $this->nombre = $data["nombre"];
+        $this->caracteristicas = $data["caracteristicas"];
+        $this->precio = $data["precio"];
+        $this->disponible = $data["disponible"];
+    }
+    
+    function getAttributes(){
+        return array(
+            "producto_id" => $this->producto_id,
+            "proveedor" => $this->proveedor,
+            "nombre" => $this->nombre,
+            "caracteristicas" => $this->caracteristicas,
+            "precio" => $this->precio,
+            "disponible" => $this->disponible,
+       );
+    }
+    
+    function insert(){
+        $statement = $this->connection->prepare("INSERT INTO Producto(producto_id, nombrePro, nombre, caracteristicas, precio, disponible) VALUES(?,?,?,?,?)");
+        $statement->bind_param("sssii", $this->proveedor, $this->nombre, $this->caracteristicas, $this->precio, $this->disponible);
         $statement->execute();
-        $result=$statement->get_result();
-        $result->fetch();
-        
-        $statement->close();
-        return $result;
-    }
-    
-    function insert($username, $password, $concessionaire){
-        $statement=$this->conn->prepare("INSERT INTO Usuario VALUES(?, ?,?)");
-        $statement->bind_param("sss",$username,$password, $concessionaire);
-        $statement->execute();
         $statement->close();
     }
-    
 }
-
-?>
